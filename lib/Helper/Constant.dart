ba39811211156a73c40eb4f8f0674b52c1a146fc
saiful <engsaiful0@gmail.com>
2022-08-31 22:19:00 +0600

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+// add your constant file's
+
+final String appName = 'Seller App eShop';
+
+final String packageName = 'eshop.seller.multivendor';
+
+final String iosPackage = 'eshop.seller.multivendor';
+
+final String baseUrl = 'https://vendor.eshopweb.store/seller/app/v1/api/';
+
+final String jwtKey = "68f05dec6014f68e760c5c5fa3e31bcf391a2e10";
+
+//timeout limit
+
+final int timeOut = 50;
+
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
